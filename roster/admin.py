571d04a07300aa6437c5dc1466987da577704b26
simone-duval <simone.teresa.duval@gmail.com>
2014-02-25 22:59:33 -0500

    done with intro_django ppt up to here

---
+++ roster/admin.py
@@ -0,0 +1,14 @@
+#FILE: roster/admin.py
+
+from django.contrib import admin
+from roster.models import Course, Student
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    
+admin.site.register(Course, CourseAdmin)
+
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    
+admin.site.register(Student, StudentAdmin)
--- roster/views.py
+++ roster/views.py
@@ -1,7 +1,11 @@
 #FILE: roster/views.py
 # Create your views here.
 from django.shortcuts import render
+from roster.models import Course, Student
 
 def home(request):
-    context = {'message': 'This is a dynamic message variable!'}
+    context = {
+        'student_count': Student.objects.count(),
+        'course_count': Course.objects.count(),
+    }
     return render(request, "base.html", context)
